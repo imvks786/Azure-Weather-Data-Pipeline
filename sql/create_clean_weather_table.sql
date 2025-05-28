1f651862ea44317d8bb41a6771581e7a15dae3d2
Vivek Singh <viveksingh9891285537@gmail.com>
2025-05-29 00:11:22 +0530

    Updates

--- sql/create_clean_weather_table.sql
+++ sql/create_clean_weather_table.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE WeatherData (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    City NVARCHAR(100),
+    Temperature FLOAT,
+    Humidity INT,
+    Pressure INT,
+    RecordedAt DATETIME DEFAULT GETDATE()
+);
